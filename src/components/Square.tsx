5982c35397820f90658e12f8128f5848b348995a
sph3 <59633047+sph3@users.noreply.github.com>
2022-03-20 18:58:09 -0300

    Add dark-mode specific colors

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -6,7 +6,7 @@ export const Square = (props: SquareProps) => {
   return (
     <div
       key={props.squareIndex}
-      className="h-14 w-14 border-4 rounded-md border-black3 bg-black3"
+      className="h-14 w-14 border-4 rounded-md dark:border-black3 dark:bg-black3"
     ></div>
   );
 };
