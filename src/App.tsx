790852f15492a67b4031848b750d79dd5cde8d26
sph3 <59633047+sph3@users.noreply.github.com>
2022-03-20 18:01:22 -0300

    Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
+import { Header } from './components/Header';
+
 export const App = () => {
   return (
     <section id="root" className="dark">
-      <div
+      <section
         id="background"
         className="h-screen flex justify-center align-center dark:bg-black2 bg-gray1"
       >
-        <div className="text-yellow font-medium text-3xl">
-          Bem-vindo(a) ao PALAVRO!
-        </div>
-      </div>
+        <Header />
+      </section>
     </section>
   );
 };
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return (
+    <section id="header" className="mt-2">
+      <h1 id="title" className="dark:text-yellow font-semibold text-3xl">
+        P<span className="dark:text-green">A</span>LAVR
+        <span className="dark:text-red">O</span>
+      </h1>
+    </section>
+  );
+};
