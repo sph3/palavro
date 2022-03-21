ba5c60c889fa7f66f43a2d36c0ed220b76534503
sph3 <59633047+sph3@users.noreply.github.com>
2022-03-21 20:48:23 -0300

    Keyboard quase pronto

--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -1,9 +1,26 @@
-import { FC } from 'react';
-
-type KeyProps = {
-  letter: string;
+type KeyProps = React.ComponentProps<'button'> & {
+  dataKey: string;
 };
 
-export const Key: FC<KeyProps> = ({ letter }) => {
-  return <span>{letter}</span>;
+export const Key = (props: KeyProps) => {
+  if (props.children) {
+    return (
+      <button className=" h-12 w-7 justify-center bg-red border-0 rounded-md">
+        {props.children}
+      </button>
+    );
+  }
+  return (
+    <>
+      {props.dataKey === 'ENTER' ? (
+        <button className="h-12 w-20 text-center bg-sky text-black1 font-medium border-0 rounded-md">
+          {props.dataKey.toUpperCase()}
+        </button>
+      ) : (
+        <button className="h-12 w-7 text-center bg-black3 text-white font-medium border-0 rounded-md">
+          {props.dataKey.toUpperCase()}
+        </button>
+      )}
+    </>
+  );
 };
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,7 +1,53 @@
+import { Key } from './Key';
+
 export const Keyboard = () => {
   return (
-    <section id="keyboard-root" className="flex flex-col justify-between">
-      <Key letter="q" />
+    <section id="keyboard-root" className="flex flex-col mt-10 gap-3">
+      <div id="row-1" className="flex flex-row justify-center gap-1">
+        <Key dataKey="q" />
+        <Key dataKey="w" />
+        <Key dataKey="e" />
+        <Key dataKey="r" />
+        <Key dataKey="t" />
+        <Key dataKey="y" />
+        <Key dataKey="u" />
+        <Key dataKey="i" />
+        <Key dataKey="o" />
+        <Key dataKey="p" />
+      </div>
+      <div id="row-2" className="flex flex-row justify-center gap-1">
+        <Key dataKey="a" />
+        <Key dataKey="s" />
+        <Key dataKey="d" />
+        <Key dataKey="f" />
+        <Key dataKey="g" />
+        <Key dataKey="h" />
+        <Key dataKey="j" />
+        <Key dataKey="k" />
+        <Key dataKey="l" />
+
+        <Key dataKey="backspace">
+          <svg
+            fill="#1A1826"
+            width={21}
+            height={20}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 640 512"
+          >
+            <path d="M576 64H205.26A63.97 63.97 0 0 0 160 82.75L9.37 233.37c-12.5 12.5-12.5 32.76 0 45.25L160 429.25c12 12 28.28 18.75 45.25 18.75H576c35.35 0 64-28.65 64-64V128c0-35.35-28.65-64-64-64zm-84.69 254.06c6.25 6.25 6.25 16.38 0 22.63l-22.62 22.62c-6.25 6.25-16.38 6.25-22.63 0L384 301.25l-62.06 62.06c-6.25 6.25-16.38 6.25-22.63 0l-22.62-22.62c-6.25-6.25-6.25-16.38 0-22.63L338.75 256l-62.06-62.06c-6.25-6.25-6.25-16.38 0-22.63l22.62-22.62c6.25-6.25 16.38-6.25 22.63 0L384 210.75l62.06-62.06c6.25-6.25 16.38-6.25 22.63 0l22.62 22.62c6.25 6.25 6.25 16.38 0 22.63L429.25 256l62.06 62.06z" />
+          </svg>
+        </Key>
+      </div>
+      <div id="row-3" className="flex flex-row justify-center gap-1">
+        <Key dataKey="z" />
+        <Key dataKey="x" />
+        <Key dataKey="c" />
+        <Key dataKey="v" />
+        <Key dataKey="b" />
+        <Key dataKey="n" />
+        <Key dataKey="m" />
+        <Key dataKey="ENTER" />
+      </div>
     </section>
   );
 };
